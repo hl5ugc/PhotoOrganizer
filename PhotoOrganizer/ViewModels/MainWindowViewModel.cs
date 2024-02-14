e122c2d4447f9fbd8d5e7eed3751fa9eb4f7216f
hl5ugc <hl5ugc@nate.com>
2024-02-14 17:41:36 +0900

    Improve MainWindow code-behind.

--- PhotoOrganizer/ViewModels/MainWindowViewModel.cs
+++ PhotoOrganizer/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,13 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Windows.Storage;
 
 namespace PhotoOrganizer.ViewModels;
 
-[ObservableObject]
-
-public partial class MainWindowViewModel
+ 
+public partial class MainWindowViewModel  : ObservableObject
 {
     [ObservableProperty]
     private StorageFolder? _inputFolder;
@@ -15,5 +17,30 @@ public partial class MainWindowViewModel
     public MainWindowViewModel()
     {
 
+    }
+
+    [RelayCommand]
+    private async Task UpdateInputFolderPath(string? folderPath)
+    {
+        if (folderPath is not null)
+        {
+            StorageFolder? folder = await StorageFolder.GetFolderFromPathAsync(folderPath);
+            if (folder is not null)
+            {
+                InputFolder = folder;
+            }
+        }
+    }
+    [RelayCommand]
+    private async Task UpdateOutputFolderPath(string? folderPath)
+    {
+        if (folderPath is not null)
+        {
+            StorageFolder? folder = await StorageFolder.GetFolderFromPathAsync(folderPath);
+            if (folder is not null)
+            {
+                OutputFolder = folder;
+            }
+        }
     }
 }
